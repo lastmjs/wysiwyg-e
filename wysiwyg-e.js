f65af15c7195a8c25cc0c95057484e454691298f
Jordan Last <jordan.michael.last@gmail.com>
2019-03-18 14:57:20 -0600

    Update wysiwyg-e.js

--- wysiwyg-e.js
+++ wysiwyg-e.js
@@ -42,7 +42,9 @@ if (document) {
 		</svg>
 	`;
 
-	document.body.appendChild(iconset);
+	window.addEventListener('load', () => {
+		document.body.appendChild(iconset);
+	});
 }
 
 export class WysiwygE extends PolymerElement {
